85875141b629650f0b17c2cf61d7259fd04ed6fc
Haarsh <hp962682@gmail.com>
2026-04-15 20:21:10 +0530

    news

---
+++ 037_logic_all/demo.py
@@ -0,0 +1,25 @@
+number = int(input("Enter the number: "))
+temp = number 
+sum = 0 
+
+# while temp > 0 :
+#     digit = temp % 10
+#     cube = digit **3
+#     sum = sum +cube
+#     temp //=10
+
+# if sum == number:
+#     print("armstornge number")
+# else:
+#     print("NOT")
+
+# while temp > 0 :
+#     digit = temp % 10
+#     sum = sum* +digit 
+#     temp //=10
+
+# if sum == number:
+#     print("palindrom number")
+# else:
+#     print("not palindorm number")
+
